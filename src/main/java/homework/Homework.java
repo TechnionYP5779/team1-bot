22a54dce4a13c1136df5bb4e25ba9853cea6bac8
Itamaror <itamar.ord@campus.technion.ac.il>
2018-12-05 23:20:00 +0200

    Moved all homework related classes into the repo #9

---
+++ src/main/java/homework/Homework.java
@@ -0,0 +1,38 @@
+package homework;
+
+import java.util.Date;
+
+public class Homework {
+  private final String courseNum;
+  private final String courseName;
+  private final Date dueDate;
+
+  public Homework(String courseNum, String courseName, Date dueDate) {
+    this.courseNum = courseNum;
+    this.courseName = courseName;
+    this.dueDate = dueDate;
+  }
+
+  public String getCourseNum() {
+    return courseNum;
+  }
+
+  public String getCourseName() {
+    return courseName;
+  }
+
+  public Date getDueDate() {
+    return dueDate;
+  }
+
+  @Override public String toString() {
+    StringBuilder $ = new StringBuilder();
+    $.append(courseName);
+    $.append(" - ");
+    $.append(courseNum);
+    $.append(" - ");
+    $.append("Due To: ");
+    $.append(dueDate);
+    return $ + "";
+  }
+}
