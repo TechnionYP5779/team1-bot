d83901048162cd0db474dae8b3af037c5c21ae8d
Itamaror <itamar.ord@campus.technion.ac.il>
2018-12-06 00:36:43 +0200

    Added getUpcomingHomework behavior to azure function #8

--- src/main/java/homework/HomeworkGetter.java
+++ src/main/java/homework/HomeworkGetter.java
@@ -1,6 +1,5 @@
 package homework;
 
-
 import java.io.*;
 import java.text.*;
 import java.util.*;
@@ -11,74 +10,73 @@ import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
 
 public class HomeworkGetter {
-  private List<Homework> homework = new ArrayList<>();
+	private List<Homework> homework = new ArrayList<>();
 
-  public HomeworkGetter(LoginCredentials creds) {
-    try {
-      java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
-      this.homework = GetHomework(creds);
-    } catch (IOException ¢) {
-      ¢.printStackTrace();
-    }
-  }
+	public HomeworkGetter(LoginCredentials creds) {
+		try {
+			java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+			this.homework = GetHomework(creds);
+		} catch (IOException ¢) {
+			¢.printStackTrace();
+		}
+	}
 
-  private static List<Homework> GetHomework(LoginCredentials c) throws IOException {
-    try (WebClient webClient = new WebClient()) {
-      HtmlPage page = webClient.getPage("https://grades.cs.technion.ac.il/grades.cgi");
-      final HtmlForm form = page.getFormByName("grform");
-      final HtmlSubmitInput button = form.getInputByName("submit");
-      final HtmlTextInput usernameField = form.getInputByName("ID");
-      final HtmlPasswordInput passwordField = form.getInputByName("Password");
-      usernameField.type(c.getUsername());
-      passwordField.type(c.getPassword());
-      final HtmlPage page2 = button.click();
-      if (page2.getUrl() == page.getUrl())
-        throw new WrongCredentialsException();
-      HtmlAnchor homeworkAnchor = page2.getAnchorByText("Homeworks");
-      HtmlPage page3 = homeworkAnchor.click();
-      final HtmlTable $ = (HtmlTable) page3.getByXPath("//table[@class='stud-hw-table']").get(0);
-      $.asText();
-      return ExtractHomeworkFromTable($);
-    }
-  }
+	private static List<Homework> GetHomework(LoginCredentials c) throws IOException {
+		try (WebClient webClient = new WebClient()) {
+			HtmlPage page = webClient.getPage("https://grades.cs.technion.ac.il/grades.cgi");
+			final HtmlForm form = page.getFormByName("grform");
+			final HtmlSubmitInput button = form.getInputByName("submit");
+			final HtmlTextInput usernameField = form.getInputByName("ID");
+			final HtmlPasswordInput passwordField = form.getInputByName("Password");
+			usernameField.type(c.getUsername());
+			passwordField.type(c.getPassword());
+			final HtmlPage page2 = button.click();
+			if (page2.getUrl() == page.getUrl())
+				throw new WrongCredentialsException();
+			HtmlPage page3 = page2.getAnchorByText("Homeworks").click();
+			final HtmlTable $ = (HtmlTable) page3.getByXPath("//table[@class='stud-hw-table']").get(0);
+			$.asText();
+			return ExtractHomeworkFromTable($);
+		}
+	}
 
-  private static List<Homework> ExtractHomeworkFromTable(HtmlTable homeworkTable) {
-    List<Homework> $ = new ArrayList<>();
-    List<HtmlTableRow> homeworkTableRows = homeworkTable.getRows();
-    for (int ¢ = 1; ¢ < homeworkTableRows.size(); ++¢) {
-      Homework h1;
-      HtmlTableRow r1 = homeworkTableRows.get(¢);
-      h1 = parseHomeworkRow(r1);
-      if (h1 != null)
-        $.add(h1);
-    }
-    return $;
-  }
+	private static List<Homework> ExtractHomeworkFromTable(HtmlTable homeworkTable) {
+		List<Homework> $ = new ArrayList<>();
+		List<HtmlTableRow> homeworkTableRows = homeworkTable.getRows();
+		for (int ¢ = 1; ¢ < homeworkTableRows.size(); ++¢) {
+			Homework h1;
+			h1 = parseHomeworkRow(homeworkTableRows.get(¢));
+			if (h1 != null)
+				$.add(h1);
+		}
+		return $;
+	}
 
-  private static Homework parseHomeworkRow(HtmlTableRow $) {
-    try {
-      String[] courseInfo = $.getCell(0).asText().split("-");
-      return $.getCell(1).asText().contains("No deadline given") ? null
-          : new Homework(courseInfo[0].trim(), courseInfo[1].trim(), (new SimpleDateFormat("dd/MM/yyyy - hh:mm")).parse($.getCell(1).asText()));
-    } catch (IndexOutOfBoundsException | ParseException ¢) {
-      ¢.printStackTrace();
-    }
-    return null;
-  }
+	private static Homework parseHomeworkRow(HtmlTableRow $) {
+		try {
+			String[] courseInfo = $.getCell(0).asText().split("-");
+			return $.getCell(1).asText().contains("No deadline given") ? null
+					: new Homework(courseInfo[0].trim(), courseInfo[1].trim(),
+							(new SimpleDateFormat("dd/MM/yyyy - hh:mm")).parse($.getCell(1).asText()));
+		} catch (IndexOutOfBoundsException | ParseException ¢) {
+			¢.printStackTrace();
+		}
+		return null;
+	}
 
-  public List<Homework> getAllHomework() {
-    return homework;
-  }
+	public List<Homework> getAllHomework() {
+		return homework;
+	}
 
-  public List<Homework> getUpcomingHomework() {
-    Date $ = new Date();
-    return homework.stream().filter(hw -> $.before(hw.getDueDate())).collect(Collectors.toList());
-  }
+	public List<Homework> getUpcomingHomework() {
+		Date $ = new Date();
+		return homework.stream().filter(hw -> $.before(hw.getDueDate())).collect(Collectors.toList());
+	}
 
-  public String getUpcomingHomeworkAsString() {
-    StringBuilder $ = new StringBuilder();
-    $.append("The upcoming homework are:\n");
-    getUpcomingHomework().forEach(λ -> $.append(λ + "\n"));
-    return $ + "";
-  }
+	public String getUpcomingHomeworkAsString() {
+		StringBuilder $ = new StringBuilder();
+		$.append("The upcoming homework are:\n");
+		getUpcomingHomework().forEach(λ -> $.append(λ + "\n"));
+		return $ + "";
+	}
 }
