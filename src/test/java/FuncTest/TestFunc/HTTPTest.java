e8a10dda5353133546d346c8e5b0d043df03d250
Ofir <ofiralexi@gmail.com>
2018-12-05 21:59:00 +0200

    removed new spartanize tips

--- src/test/java/FuncTest/TestFunc/HTTPTest.java
+++ src/test/java/FuncTest/TestFunc/HTTPTest.java
@@ -1,14 +1,5 @@
 package FuncTest.TestFunc;
 
-//import java.util.HashMap;
-
-import org.junit.Test;
-
-import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.HttpStatusType;
-
-import FuncTest.TestFunc.Function;
-
 public class HTTPTest {
 
 	
