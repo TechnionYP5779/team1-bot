4337df1d1eeedf8f43d2286a43ffd427431d5404
Ofir <ofiralexi@gmail.com>
2018-12-25 01:39:07 +0200

    removed spartanized tips

--- src/main/java/rule/subscribeHandler.java
+++ src/main/java/rule/subscribeHandler.java
@@ -2,9 +2,6 @@ package rule;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import FuncTest.TestFunc.globals;
 
