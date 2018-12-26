72b5d8ea77dfd2c1fdd98482a5955f5a5934940f
Matan <matan.pugach@campus.technion.ac.il>
2018-12-26 16:05:49 +0200

    Added randomized answering #59

--- src/main/java/help/BotFeaturesInfo.java
+++ src/main/java/help/BotFeaturesInfo.java
@@ -1,6 +1,9 @@
 package help;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import org.json.JSONObject;
 
@@ -11,17 +14,22 @@ import com.microsoft.azure.functions.HttpResponseMessage;
 import FuncTest.TestFunc.utils;
 
 public class BotFeaturesInfo {
-	
+
 	public static String WELCOME_MESSAGE = "Welcome to the TechnoBot - the first Technion-Bot!\r\n";
-	public static String AVAILABLE_ACTIONS = "The TechnoBot can do the following:\r\n" + 
-			"1. Keep track of homework assignments\r\n\ttry: 'When is my next assigment due'\r\n" + 
-			"2. Guide to attractions around campus\r\n\ttry: 'when can I go to Junta'\r\n" +
-			"3. Search for videos relavant to a course\r\n\ttry:'does course 234319 have a video?'\r\n" +
-			"5. Filter courses based on the given info\r\n\ttry: 'filter courses'\r\n" + 
-			"6. Search for info on courses \r\n\ttry: 'Search class'\r\n" + 
-			"7. Retrive both prerequisites & postrequisites \r\n\ttry:'Give me the prerequisites of Information Retrieval' or 'What are the postrequisites of Computer Vision?'"+
-			"8. Define your own rules & execute them\r\n\ttry:'subscribe to rule homework with username <uname> and password <pwd>' and 'run my rules'"+
-			"If you are having problems, please respond \"support\"";
+	public static String[] AVAILABLE_PREFIXES = { "The TechnoBot can do the following:\r\n", "I can help you with:\r\n",
+			"I am capable of the following:\r\n", "I can do plenty:\r\n" };
+	private static final String[] FEATURES_ARRAY = {
+			"1. Keep track of homework assignments\r\n\ttry: 'When is my next assigment due'\r\n",
+			"2. Guide to attractions around campus\r\n\ttry: 'when can I go to Junta'\r\n",
+			"3. Search for videos relavant to a course\r\n\ttry:'does course 234319 have a video?'\r\n",
+			"5. Filter courses based on the given info\r\n\ttry: 'filter courses'\r\n",
+			"6. Search for info on courses \r\n\ttry: 'Search class'\r\n",
+			"7. Retrive both prerequisites & postrequisites \r\n\ttry:'Give me the prerequisites of Information Retrieval' or 'What are the postrequisites of Computer Vision?'",
+			"8. Define your own rules & execute them\r\n\ttry:'subscribe to rule homework with username <uname> and password <pwd>' and 'run my rules'", };
+
+	public static String[] AVAILABLE_POSTFIXES = { "If you are having problems, please respond \"support\"",
+			"In case you encounter an issue, please respond \"support\"",
+			"For further guidance, please respond \"support\"","You can get more info by saying \"support\"" };
 
 	public static HttpResponseMessage returnInfoResponse(JSONObject queryResult, HttpRequestMessage<Optional<String>> s,
 			ExecutionContext c) {
@@ -29,6 +37,20 @@ public class BotFeaturesInfo {
 	}
 
 	private static String buildInfoText() {
-		return WELCOME_MESSAGE+AVAILABLE_ACTIONS;
+		int prefix_index = new Random().nextInt(AVAILABLE_PREFIXES.length);
+		int postfix_index = new Random().nextInt(AVAILABLE_PREFIXES.length);
+
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < FEATURES_ARRAY.length; i++) {
+			list.add(FEATURES_ARRAY[i]);
+		}
+		java.util.Collections.shuffle(list);
+		int count = 1;
+		String capabilites = "";
+		for (String s : list) {
+			capabilites += count + ". " + s;
+			count++;
+		}
+		return WELCOME_MESSAGE + AVAILABLE_PREFIXES[prefix_index] + capabilites + AVAILABLE_POSTFIXES[postfix_index];
 	}
 }
