d16e28092d01de93a83852caf2706b7923e1b122
yoavz <yoavzuriel@cs.technion.ac.il>
2018-12-27 23:42:31 +0200

    removed stopwords.txt and inserted them directly to the class #56

--- src/main/java/indexer/CourseSimularitySearcher.java
+++ src/main/java/indexer/CourseSimularitySearcher.java
@@ -2,7 +2,6 @@ package indexer;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.AbstractMap;
@@ -19,19 +18,54 @@ public class CourseSimularitySearcher {
 	private HashMap<Integer, BagOfWords> coursesSyllabi = new HashMap<>();
 	private BagOfWords corpus = new BagOfWords();
 
-	private void initStopwords(String swPath) throws FileNotFoundException, IOException {
-		try (BufferedReader br = new BufferedReader(new FileReader(new File(swPath)))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				this.stopwords.addAll(Arrays.asList(line.split(",")));
-			}
-		}
+	private void initStopwords(){
+		this.stopwords.addAll(Arrays.asList("a", "about", "above", "according", "across", "after", "afterwards",
+				"again", "against", "albeit", "all", "almost", "alone", "along", "already", "also", "although",
+				"always", "among", "amongst", "am", "an", "and", "another", "any", "anybody", "anyhow", "anyone",
+				"anything", "anyway", "anywhere", "apart", "are", "around", "as", "at", "av", "be", "became", "because",
+				"become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside",
+				"besides", "between", "beyond", "both", "but", "by", "can", "cannot", "canst", "certain", "cf",
+				"choose", "contrariwise", "cos", "could", "cu", "day", "do", "does", "doesn't", "doing", "dost", "doth",
+				"double", "down", "dual", "during", "each", "either", "else", "elsewhere", "enough", "et", "etc",
+				"even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "except", "excepted",
+				"excepting", "exception", "exclude", "excluding", "exclusive", "far", "farther", "farthest", "few",
+				"ff", "first", "for", "formerly", "forth", "forward", "from", "front", "further", "furthermore",
+				"furthest", "get", "go", "had", "halves", "hardly", "has", "hast", "hath", "have", "he", "hence",
+				"henceforth", "her", "here", "hereabouts", "hereafter", "hereby", "herein", "hereto", "hereupon",
+				"hers", "herself", "him", "himself", "hindmost", "his", "hither", "hitherto", "how", "however",
+				"howsoever", "i", "ie", "if", "in", "inasmuch", "inc", "include", "included", "including", "indeed",
+				"indoors", "inside", "insomuch", "instead", "into", "inward", "inwards", "is", "it", "its", "itself",
+				"just", "kind", "kg", "km", "last", "latter", "latterly", "less", "lest", "let", "like", "little",
+				"ltd", "many", "may", "maybe", "me", "meantime", "meanwhile", "might", "moreover", "most", "mostly",
+				"more", "mr", "mrs", "ms", "much", "must", "my", "myself", "namely", "need", "neither", "never",
+				"nevertheless", "next", "no", "nobody", "none", "nonetheless", "noone", "nope", "nor", "not", "nothing",
+				"notwithstanding", "now", "nowadays", "nowhere", "of", "off", "often", "ok", "on", "once", "one",
+				"only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out",
+				"outside", "over", "own", "per", "perhaps", "plenty", "provide", "quite", "rather", "really", "round",
+				"said", "sake", "same", "sang", "save", "saw", "see", "seeing", "seem", "seemed", "seeming", "seems",
+				"seen", "seldom", "selves", "sent", "several", "shalt", "she", "should", "shown", "sideways", "since",
+				"slept", "slew", "slung", "slunk", "smote", "so", "some", "somebody", "somehow", "someone", "something",
+				"sometime", "sometimes", "somewhat", "somewhere", "spake", "spat", "spoke", "spoken", "sprang",
+				"sprung", "stave", "staves", "still", "such", "supposing", "than", "that", "the", "thee", "their",
+				"them", "themselves", "then", "thence", "thenceforth", "there", "thereabout", "thereabouts",
+				"thereafter", "thereby", "therefore", "therein", "thereof", "thereon", "thereto", "thereupon", "these",
+				"they", "this", "those", "thou", "though", "thrice", "through", "throughout", "thru", "thus", "thy",
+				"thyself", "till", "to", "together", "too", "toward", "towards", "ugh", "unable", "under", "underneath",
+				"unless", "unlike", "until", "up", "upon", "upward", "upwards", "us", "use", "used", "using", "very",
+				"via", "vs", "want", "was", "we", "week", "well", "were", "what", "whatever", "whatsoever", "when",
+				"whence", "whenever", "whensoever", "where", "whereabouts", "whereafter", "whereas", "whereat",
+				"whereby", "wherefore", "wherefrom", "wherein", "whereinto", "whereof", "whereon", "wheresoever",
+				"whereto", "whereunto", "whereupon", "wherever", "wherewith", "whether", "whew", "which", "whichever",
+				"whichsoever", "while", "whilst", "whither", "who", "whoa", "whoever", "whole", "whom", "whomever",
+				"whomsoever", "whose", "whosoever", "why", "will", "wilt", "with", "within", "without", "worse",
+				"worst", "would", "wow", "ye", "yet", "year", "yippee", "you", "your", "yours", "yourself",
+				"yourselves"));
 	}
 
 	public CourseSimularitySearcher initCourses(String descPath, String swPath) {
 		try {
 			if (swPath != null)
-				initStopwords(swPath);
+				initStopwords();
 			try (BufferedReader br = new BufferedReader(new FileReader(new File(descPath)))) {
 				String line;
 				Integer courseNumber = Integer.valueOf(-1);
